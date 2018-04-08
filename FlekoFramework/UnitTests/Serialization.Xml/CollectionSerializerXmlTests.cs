980ffdd8d66c63ea622fc11615f45f3ad25f8172
AlexandrSurkov <AOSurkov@gmail.com>
2018-04-08 23:07:45 +0300

    XML serializers minor updates

--- FlekoFramework/UnitTests/Serialization.Xml/CollectionSerializerXmlTests.cs
+++ FlekoFramework/UnitTests/Serialization.Xml/CollectionSerializerXmlTests.cs
@@ -41,6 +41,27 @@ namespace Flekosoft.UnitTests.Serialization.Xml
             Assert.IsNotNull(s.XmlRoot);
             Assert.AreEqual(text,s.XmlRoot.Name);
 
+
+            var element = s.XmlDocument.CreateElement("element1");
+            s.XmlRoot.AppendChild(element);
+            Assert.AreEqual(1, s.XmlRoot.ChildNodes.Count);
+
+            element = s.XmlDocument.CreateElement("element2");
+            s.XmlRoot.AppendChild(element);
+            Assert.AreEqual(2, s.XmlRoot.ChildNodes.Count);
+
+            s.XmlRoot.RemoveChild(element);
+            Assert.AreEqual(1, s.XmlRoot.ChildNodes.Count);
+
+            element = s.XmlDocument.CreateElement("element3");
+            s.XmlRoot.AppendChild(element);
+            Assert.AreEqual(2, s.XmlRoot.ChildNodes.Count);
+
+            s.XmlRoot.RemoveAll();
+            Assert.AreEqual(0, s.XmlRoot.ChildNodes.Count);
+
+            s.Dispose();
+
         }
     }
 }
--- FlekoFramework/UnitTests/Serialization.Xml/SerializerXmlTests.cs
+++ FlekoFramework/UnitTests/Serialization.Xml/SerializerXmlTests.cs
@@ -34,12 +34,33 @@ namespace Flekosoft.UnitTests.Serialization.Xml
         {
             var item = new SerializerTestItem();
             var text = "123sdf";
-            var s = new TestXmlSerializer(item,text);
+            var s = new TestXmlSerializer(item, text);
 
             Assert.IsNotNull(s.XmlDocument);
 
             Assert.IsNotNull(s.XmlRoot);
-            Assert.AreEqual(text,s.XmlRoot.Name);
+            Assert.AreEqual(text, s.XmlRoot.Name);
+
+
+            var element = s.XmlDocument.CreateElement("element1");
+            s.XmlRoot.AppendChild(element);
+            Assert.AreEqual(1, s.XmlRoot.ChildNodes.Count);
+
+            element = s.XmlDocument.CreateElement("element2");
+            s.XmlRoot.AppendChild(element);
+            Assert.AreEqual(2, s.XmlRoot.ChildNodes.Count);
+
+            s.XmlRoot.RemoveChild(element);
+            Assert.AreEqual(1, s.XmlRoot.ChildNodes.Count);
+
+            element = s.XmlDocument.CreateElement("element3");
+            s.XmlRoot.AppendChild(element);
+            Assert.AreEqual(2, s.XmlRoot.ChildNodes.Count);
+
+            s.XmlRoot.RemoveAll();
+            Assert.AreEqual(0, s.XmlRoot.ChildNodes.Count);
+
+            s.Dispose();
 
         }
     }
